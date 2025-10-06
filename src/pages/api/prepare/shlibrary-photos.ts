256bacae4fd2f7542c972e6debabf1c6102b8eb4
hui5 <huiwu02@gmail.com>
2025-10-06 16:18:10 +0800

    add biome.json

--- src/pages/api/prepare/shlibrary-photos.ts
+++ src/pages/api/prepare/shlibrary-photos.ts
@@ -28,7 +28,7 @@ export default async function handler(
 			pageSize: 100,
 		}).then((r) => r.datas ?? null);
 		return res.status(200).json({ photos });
-	} catch (e) {
+	} catch (_e) {
 		return res.status(500).json({ error: "Internal Server Error" });
 	}
 }
