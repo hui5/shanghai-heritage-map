556dd0d62df311d374db3edef0cf50abad267005
hui5 <huiwu02@gmail.com>
2025-10-13 00:00:54 +0800

    使用 works： analytics-proxy-vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
